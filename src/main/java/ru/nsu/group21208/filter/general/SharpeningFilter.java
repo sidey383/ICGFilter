fcb9050c87a61dab68a703216d3ff0761de044b9
johnkonstantin <danilajjj@mail.ru>
2024-03-31 02:14:40 +0700

    Change parameters to empty

--- src/main/java/ru/nsu/group21208/filter/general/SharpeningFilter.java
+++ src/main/java/ru/nsu/group21208/filter/general/SharpeningFilter.java
@@ -2,16 +2,14 @@ package ru.nsu.group21208.filter.general;
 
 import ru.nsu.group21208.filter.FilterEditor;
 import ru.nsu.group21208.filter.ImageTransformation;
-import ru.nsu.group21208.filter.base.BaseFilterEditor;
-import ru.nsu.group21208.filter.base.BaseFilterParams;
-import ru.nsu.group21208.filter.base.param.IntegerParam;
+import ru.nsu.group21208.filter.base.EmptyEditor;
+import ru.nsu.group21208.filter.base.EmptyParams;
 import ru.nsu.group21208.filter.kernel.KernelFilter;
 import ru.nsu.group21208.filter.kernel.edge.CopyEdgeSupplier;
 
 import java.awt.image.BufferedImage;
 
-public class SharpeningFilter extends KernelFilter<BaseFilterParams> {
-    private final BaseFilterEditor editor = new BaseFilterEditor();
+public class SharpeningFilter extends KernelFilter<EmptyParams> {
 
     private final CopyEdgeSupplier<?> copy = new CopyEdgeSupplier<>();
 
@@ -21,37 +19,37 @@ public class SharpeningFilter extends KernelFilter<BaseFilterParams> {
             {0, -1, 0}};
 
     @Override
-    public FilterEditor<BaseFilterParams> createFilterEditor() {
-        return editor;
+    public FilterEditor<EmptyParams> createFilterEditor() {
+        return EmptyEditor.getInstance();
     }
 
     @Override
-    protected int[][] kernel(BaseFilterParams parameters) {
+    protected int[][] kernel(EmptyParams parameters) {
         return kernel;
     }
 
     @Override
-    protected int kernelDivider(BaseFilterParams parameters) {
+    protected int kernelDivider(EmptyParams parameters) {
         return 4;
     }
 
     @Override
-    protected int getEdgeColor(BufferedImage image, int x, int y, BaseFilterParams parameters) {
+    protected int getEdgeColor(BufferedImage image, int x, int y, EmptyParams parameters) {
         return copy.getEdgeColor(image, x, y, null);
     }
 
     @Override
-    protected boolean isWorkOnEdge(BaseFilterParams parameters) {
+    protected boolean isWorkOnEdge(EmptyParams parameters) {
         return true;
     }
 
     @Override
-    public ImageTransformation apply(BaseFilterParams parameters) {
+    public ImageTransformation apply(EmptyParams parameters) {
         return new SharpeningFilterTransformation(parameters);
     }
 
     protected class SharpeningFilterTransformation extends KernelTransformation {
-        public SharpeningFilterTransformation(BaseFilterParams params) {
+        public SharpeningFilterTransformation(EmptyParams params) {
             super(params);
         }
 
